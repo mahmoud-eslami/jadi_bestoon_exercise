d606176f850c69ada3f6315eecc7d0ade7cedf3f
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-05-09 00:06:08 +0430

    add new model

--- web/models.py
+++ web/models.py
@@ -23,3 +23,13 @@ class Income(models.Model):
 
     def  __str__(self):
         return self.text
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=200,blank=False,null=False)
+    content = models.CharField(max_length=800,blank=False,null=False)
+    photo = models.ImageField()
+
+    def __str__(self):
+        return self.title
--- web/views.py
+++ web/views.py
@@ -5,6 +5,19 @@ from django.views.decorators.csrf import  csrf_exempt
 from web.models import  *
 from datetime import  datetime
 from django.contrib.auth.models import User
+from rest_framework.views import  APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+
 
 @csrf_exempt
 def submit_expense(request):
